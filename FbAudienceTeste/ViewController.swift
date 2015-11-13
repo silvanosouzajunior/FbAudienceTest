b04071818d8e696d1d09bb7db85b7a057ccbe65b
Silvano Souza Junior <vntsiju@suportes-imac-3.venturus.local>
2015-11-13 16:34:56 -0200

    Facebook Audience Integration

--- FbAudienceTeste/ViewController.swift
+++ FbAudienceTeste/ViewController.swift
@@ -12,14 +12,29 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let adView = FBAdView.init(placementID: "XXXXX-XXXXX", adSize: kFBAdSizeHeight50Banner, rootViewController: self)
+        adView.delegate = self
+        
+        print(adView)
+
+        adView.loadAd()
+        
+        self.view.addSubview(adView)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
+extension ViewController: FBAdViewDelegate {
+    func adViewDidLoad(adView: FBAdView) {
+        print("AdViewDidLoad")
+    }
+    
+    func adView(adView: FBAdView, didFailWithError error: NSError) {
+        print(error)
+    }
+}
