f32070462012d324a8c4a047962eff4b96a9f090
Silvano Souza Junior <vntsiju@suportes-imac-3.venturus.local>
2015-11-13 13:35:39 -0200

    Initial Commit

---
+++ FbAudienceTeste/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FbAudienceTeste
+//
+//  Created by Silvano Souza Junior on 11/13/15.
+//  Copyright © 2015 Silvano Souza Junior. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
